f6fb6a7b51aa91dee18756d851a68ef9858be171
elizabeth.a.wells <elizabeth.a.wells1@gmail.com>
2019-06-24 08:30:28 -0500

    refactor quality increases to separate method

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -26,27 +26,18 @@ namespace csharpcore
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    IncreaseQuality(Items[i]);
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].SellIn < 11)
                         {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                            IncreaseQuality(Items[i]);
+                        }
 
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                        if (Items[i].SellIn < 6)
+                        {
+                            IncreaseQuality(Items[i]);
+
                         }
                     }
                 }
@@ -77,12 +68,17 @@ namespace csharpcore
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        IncreaseQuality(Items[i]);
                     }
                 }
+            }
+        }
+
+        private void IncreaseQuality(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
             }
         }
     }
